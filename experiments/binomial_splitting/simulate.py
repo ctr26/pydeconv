af6e93132da0857b1efd0daaab538cee10ccd2e6
Craig Russell <craig.russell.phd@gmail.com>
2023-07-17 20:55:43 +0100

    this experiment file will change a lot

--- experiments/binomial_splitting/simulate.py
+++ experiments/binomial_splitting/simulate.py
@@ -2,10 +2,16 @@ import argparse
 import matplotlib
 import numpy as np
 from pydeconv.simulate import SimulateImagePoisson
-from pydeconv import deconvolve
+from pydeconv import deconvolve, optics, utils
 import matplotlib.pyplot as plt
 import binomial_splitting as bs
 from tqdm import tqdm
+from pydeconv.deconvolve import RichardsonLucy
+
+from pydeconv.metrics import ReconstructionMetrics2D
+from pydeconv import metrics
+
+
 def main(
     numPixel=(256, 256),
     midPos=(128, 128),
@@ -51,16 +57,55 @@ def main(
     )
     obj = simulator.get_object()
     noisy_obj = simulator.simulate()
-    rl_steps = np.empty((niter, *noisy_obj.shape))
-    bs_steps = np.empty((niter, *noisy_obj.shape))
-    rl_steps[0] = noisy_obj
-    V,T = bs.split(noisy_obj)
-    bs_steps[0] = V 
-    
-    for i,data in tqdm(enumerate(rl_steps[:-1])):
-        rl_steps[i] = deconvolve.richarson_lucy_step(rl_steps[i-1], obj, simulator.fwd, simulator.bwd)
-        bs_steps[i] = deconvolve.richarson_lucy_step(rl_steps[i-1], obj, simulator.fwd, simulator.bwd)
-        # bs_steps[i] = bs.binomial_splitting_step(rl_steps[i-1], obj, simulator.fwd, simulator.bwd, coin_flip_bias=coin_flip_bias)
-        
+    psf, otf, fwd, bwd = simulator.get_optical_operators()
+
+    V, T = bs.split(noisy_obj, p=coin_flip_bias)
+    objs = np.stack((noisy_obj, V, T), axis=0)
+    # rl_steps = np.expand_dims(objs, 0).repeat(niter, axis=0)
+    # rl_steps[0] = objs
+    # rl = RichardsonLucy(psf, iterations=niter, early_stopping=None)
+    # rl_steps = rl.deconvolve(objs, history=True)
+    iterations = np.arange(0, niter)
+
+    # Richardson-Lucy deconvolution of split data
+    image = fwd(objs[0])
+    est = np.ones_like(image) + np.mean(objs[0])
+    rl_steps = np.zeros_like(est)
+    rl_steps = np.repeat(rl_steps[np.newaxis], niter, axis=0)
+    for l in tqdm(iterations, desc=f"Richardson lucy deconvolution"):
+        convEst = fwd(est)
+        ratio = image / (convEst + 1e-12)
+        convRatio = bwd(ratio)
+        convRatio = convRatio / bwd(np.ones_like(image))
+        est = est * convRatio
+
+        rl_steps[l] = est
+
+    # Richardson-Lucy deconvolution of split data
+    # for i, data in tqdm(enumerate(rl_steps[:-1])):
+    #     rl_steps[i] = deconvolve.richardson_lucy.step(
+    #         rl_steps[i - 1], obj, simulator.fwd, simulator.bwd
+    #     )
+    #     # bs_steps[i] = bs.binomial_splitting_step(rl_steps[i-1], obj, simulator.fwd, simulator.bwd, coin_flip_bias=coin_flip_bias)
+    recon_metrics = ReconstructionMetrics2D(
+        x_est=rl_steps, y_est=fwd(rl_steps), x_gt=obj, y_gt=fwd(obj)
+    )
+    metrics_dict = metrics.get_metrics_dict(
+        [
+            recon_metrics.loglikelihood,
+            recon_metrics.poisson,
+            # recon_metrics.ncc,
+            recon_metrics.crossentropy,
+            recon_metrics.kl_noiseless_signal,
+        ]
+    )
+    metrics_dict
+    kl_est_noiseless_signal = np.sum(
+        fwd(obj) * np.log((fwd(obj) + 1e-9) / (fwd(rl_steps) + 1e-9)),
+        axis=(-2, -1),
+    )
+
+    rl_steps
+
 
 main()
